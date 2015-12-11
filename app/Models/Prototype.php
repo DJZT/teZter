540ff9ce38d0a0ebef720becc12069948d645c9b
Pochepko Stanislav <DJZT44@gmail.com>
2015-12-11 17:53:06 +0200

    ---

--- app/Models/Prototype.php
+++ app/Models/Prototype.php
@@ -4,6 +4,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prototype extends Model {
 
-	//
+	protected $fillable = ['title'];
+
+    public function tests(){
+        $this->hasMany(Test::class);
+    }
+
+    public function questions(){
+        $this->hasMany(Question::class);
+    }
+
 
 }
